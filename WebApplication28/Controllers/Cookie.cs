3131ab9bfd0087aa589d44d07d60c4bc286a3573
Fraidy Jacoby <fraidyjacoby@gmail.com>
2020-10-19 14:27:58 -0400

    Add project files.

---
+++ WebApplication28/Controllers/Cookie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication28.Controllers
+{
+    public class Cookie : Controller
+    {
+        private int _num;
+
+        public IActionResult Index()
+        {
+            
+            string value = Request.Cookies["numberVisit"];
+            bool alreadyHere = !String.IsNullOrEmpty(value);
+
+            if (alreadyHere)
+            {
+                int times = int.Parse(value) + 1;
+                Response.Cookies.Append("numberVisit", $"{times}");
+                _num = times;
+            }
+            else
+            {
+                _num = 1;
+                Response.Cookies.Append("numberVisit", $"{_num}");
+            }
+
+            return View(_num);
+        }
+    }
+}
